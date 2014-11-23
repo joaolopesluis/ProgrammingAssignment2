4079b331a0f3e968ebf9960160f5c8dff268b7f6
Joao Lopes Luis <joaolopesluis@yahoo.com>
2014-11-23 22:44:35 +0000

    Final version of Assigment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+		## Creates a special "vector", which is a list containing a function to
+		##
+		##1.  set the matrix
+		##2.  get the matrix
+		##3.  set the inverse matrix
+		##4.  get the inverse matrix
+		m <- NULL
+		set <- function(y) {
+				x <<- y
+				m <<- NULL
+		}
+		get <- function() x
+		setinverse <- function(solve) m <<- solve
+		getinverse <- function() m
+		list(set = set, get = get,
+			 setinverse = setinverse,
+			 getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Returns a matrix that is the inverse of 'x' only if it is stored in cache.
+		m <- x$getinverse()
+		if(!is.null(m)) {
+				message("getting cached data")
+				return(m)
+		}
+		data <- x$get()
+		m <- solve(data, ...)
+		x$setinverse(m)
+		m
 }
